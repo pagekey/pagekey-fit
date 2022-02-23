c8790c4bd51ee72952f7e3908e5a1cfea1e1bbcf
Stephen Grice <stephengrice@live.com>
2022-02-22 20:02:02 -0500

    Convert name to com.pagekeysolutions.pfl

--- android/app/src/main/kotlin/com/pagekeysolutions/pfl/MainActivity.kt
+++ android/app/src/main/kotlin/com/pagekeysolutions/pfl/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.pfl
+package com.pagekeysolutions.pfl
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:googleapis/drive/v3.dart' as drive;
+import 'package:google_sign_in/google_sign_in.dart' as signIn;
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'PFL Fitness',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,9 +24,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'PFL Fitness'),
     );
   }
 }
