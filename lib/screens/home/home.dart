d419693ee3380fd0d0abb3ff24d625c3fd61113e
Stephen Grice <stephengrice@live.com>
2022-02-25 15:02:16 -0500

    Get rid of Google Drive stuff and clean up Home

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<HomeScreen> createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Hello World',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
